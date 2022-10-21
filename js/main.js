d48c6752ba0c9c7dc382d260a87db5c964522953
dariovazzano <98060971+dariovazzano@users.noreply.github.com>
2022-10-20 22:36:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// cerrar menu cuando haces click en un item del menu
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
